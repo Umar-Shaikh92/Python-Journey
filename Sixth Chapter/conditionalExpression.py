dd21914b0c574b9f62d3f272d10707169432b6b6
Umar-Shaikh92 <umarshaikh66021@gmail.com>
2026-02-19 17:29:00 +0500

    Added Sixth Chapter

---
+++ Sixth Chapter/conditionalExpression.py
@@ -0,0 +1,42 @@
+# Quick Quize
+userInput = int(input("Enter your age :"))
+
+if(userInput>=18):
+    print("Yes")
+else:
+    print("No")
+
+
+
+# RELATIONAL/COMPARISION OPERATORS
+# ==	equal
+# !=	not equal
+# >	greater than
+# <	less than
+# >=	greater or equal
+# <=	less or equal
+
+
+
+# LOGICAL OPERATOR
+# and	 ---  both true
+# or  ---  at least one true
+# not  ---  reverse condition
+
+
+
+# age = 21
+
+# if age >= 18 and age <= 60:
+#     print("Working age")
+# else:
+#     print("Study age") 
+    
+    
+    
+    
+    
+# Conditional Expression (Ternary Operator)
+# age = 20
+# status = "Adult" if age >= 18 else "Minor"
+# print(status)
---
+++ Sixth Chapter/practiceSet.py
@@ -0,0 +1,71 @@
+# Write a program to find the greatest of four numbers entered by the user.
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# c = int(input("Enter third number: "))
+# d = int(input("Enter fourth number: "))
+
+# if a >= b and a >= c and a >= d:
+#     print("Greatest number is:", a)
+
+# elif b >= a and b >= c and b >= d:
+#     print("Greatest number is:", b)
+
+# elif c >= a and c >= b and c >= d:
+#     print("Greatest number is:", c)
+
+# else:
+#     print("Greatest number is:", d)
+
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# c = int(input("Enter third number: "))
+# d = int(input("Enter fourth number: "))
+
+# print("Greatest number is:", max(a, b, c, d))
+
+
+
+
+
+# A spam comment is defined as a text containing following keywords:
+# “Make a lot of money”, “buy now”, “subscribe this”, “click this”. Write a program
+# to detect these spams.
+
+# comment = input("Enter your comment: ")
+
+# if ("make a lot of money" in comment.lower() or
+#     "buy now" in comment.lower() or
+#     "subscribe this" in comment.lower() or
+#     "click this" in comment.lower()):
+#     print("Spam")
+# else:
+#     print("Not Spam")
+
+
+
+
+
+# Write a program to find whether a given username contains less than 10 characters or not.
+
+# username = input("Enter a username: ")
+
+# if len(username) < 10:
+#     print("Username contains less than 10 characters")
+# else:
+#     print("Username contains 10 or more characters")
+
+
+
+
+
+# Write a program to calculate the grade of a student from his marks from the
+# following scheme:
+# 90 – 100 => Ex
+# 80 – 90 => A
+# 70 – 80 => B
+# 60 – 70 =>C
+# 50 – 60 => D
+# <50 => F
+
